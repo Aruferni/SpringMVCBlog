c8f11aff06d52f66a2e362de484985fd15d11f3b
albert <albert.ferni@gmail.com>
2018-03-01 13:20:28 +0100

    1/3/18

---
+++ src/main/java/blog/BlogMvcApplication.java
@@ -0,0 +1,11 @@
+package blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMvcApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BlogMvcApplication.class, args);
+    }
+}
