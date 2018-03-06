f77d528468dc24a87bfcb58cc7b1b5a40905d294
albert <albert.ferni@gmail.com>
2018-03-06 08:51:58 +0100

    5/3/18 20:00

---
+++ src/main/java/blog/services/PostService.java
@@ -0,0 +1,14 @@
+package blog.services;
+
+import blog.models.Post;
+
+import java.util.List;
+
+public interface PostService {
+    List<Post> findAll();
+    List<Post> findLatest5();
+    Post findById(Long id);
+    Post create(Post post);
+    Post edit(Post post);
+    void deleteById(Long id);
+}
