42be0579103edbef6778073cd29d4867f1fa9f5e
albert <albert.ferni@gmail.com>
2018-03-06 11:10:28 +0100

    06/03/2018 11:10

---
+++ src/main/java/blog/controllers/PostsController.java
@@ -0,0 +1,30 @@
+package blog.controllers;
+
+import blog.models.Post;
+import blog.services.NotificationService;
+import blog.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PostsController {
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private NotificationService notifyService;
+
+    @RequestMapping("/posts/view/{id}")
+    public String view(@PathVariable("id") Long id, Model model){
+        Post post=postService.findById(id);
+        if (post == null){
+            notifyService.addErrorMessage("Cannot find post #" + id);
+            return "redirect:/";
+        }
+        model.addAttribute("post", post);
+        return "posts/view";
+    }
+}
